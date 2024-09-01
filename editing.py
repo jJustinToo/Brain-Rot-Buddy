acc1845c17ec4fcbe17107393e11afe7be8895a6
JustinToo <justintoons13@gmail.com>
2024-09-01 13:10:19 +1000

    optimised editing.py

--- editing.py
+++ editing.py
@@ -1,27 +1,20 @@
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip, AudioFileClip
+from moviepy.editor import VideoFileClip, AudioFileClip
 import random
 
-def editRedditStory(background_video, tts, output, usr_input):
-    # Load the video file
-    
-    # with AudioFileClip(audio_file) as audio_clip:
-    #     # Get the duration of the audio clip (in seconds)
-    #     duration = audio_clip.duration
-
-    audio_clip = AudioFileClip(tts)
-    video_clip = VideoFileClip(background_video)
-    
-    rnd_time = random.randint(0, int(video_clip.duration))
-    video_clip = video_clip.subclip(rnd_time,rnd_time + audio_clip.duration)
-    
-    video_clip = video_clip.set_audio(audio_clip)
-
-    # Write the result to a file
-    video_clip.write_videofile(output, codec="libx264", audio_codec="aac")
-
-    # Close the clips to release resources
-    video_clip.close()
-    audio_clip.close()
+def editRedditStory(background_video_path: str, tts_path: str, output_path: str, usr_input: list = {}):
+    with AudioFileClip(tts_path) as audio_clip, VideoFileClip(background_video_path) as video_clip:
+        
+        # Select a random start time for the video subclip
+        rnd_time = random.randint(0, int(video_clip.duration - audio_clip.duration))
+        
+        # Create a subclip of the video with the same duration as the audio
+        sub_video_clip = video_clip.subclip(rnd_time, rnd_time + audio_clip.duration)
+        
+        # Set the audio to the subclip
+        final_clip = sub_video_clip.set_audio(audio_clip)
+        
+        # Write the final video to a file using multithreading
+        final_clip.write_videofile(output_path, codec="libx264", audio_codec="aac", threads=6)
 
 # editRedditStory("resources/background.mov", "resources/output.mp3", "output/output_video.mp4", 0)
 
