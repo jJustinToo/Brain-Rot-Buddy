2f24babc6362bfc8e6f9fadf2645d7f403227b7d
JustinToo <justintoons13@gmail.com>
2024-09-01 13:10:04 +1000

    changed to gemma2:2b model

--- chatbot.py
+++ chatbot.py
@@ -1,19 +1,16 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
-model = OllamaLLM(model="llama3.1")
-def generateRedditStory(topic):
-    print("Generating reddit story text...")
-    template = """
-    Create a story based on the topic below
+# model = OllamaLLM(model="llama3.1")
+model = OllamaLLM(model="gemma2:2b")
 
-    Please limit the story to 125 words but also generate atleast 100 words. This is a hard rule.
-    
-    Topic: {topic}
+templates = {
+    "redditStory": """Create a story based on the topic below\n\nPlease limit the story to 125 words but also generate atleast 100 words. This is a hard rule. Please refrain from writing any filler from your reply like "This is a good prompt". Please only return the story's text. I do not want any other information such as the word count.\n\nTopic: {topic}\n\nStory: """
+}
 
-    Story: 
-    """
-    prompt = ChatPromptTemplate.from_template(template)
+def generateRedditStory(topic: str):
+    print(f'Generating a reddit story about "{topic}"...')
+    prompt = ChatPromptTemplate.from_template(templates["redditStory"])
     chain = prompt | model  
 
     result = chain.invoke({"topic": topic})
@@ -22,19 +19,3 @@ def generateRedditStory(topic):
 
 def produceImages(topic):
     pass
-    print("Generating text...")
-    template = """
-    Create a story based on the topic below
-
-    Please limit the story to 125 words but also generate atleast 100 words. This is a hard rule.
-    
-    Topic: {topic}
-
-    Story: 
-    """
-    prompt = ChatPromptTemplate.from_template(template)
-    chain = prompt | model  
-
-    result = chain.invoke({"topic": topic})
-    print(f'Text for a reddit story based on "{topic}" has been generated succesfully.')
-    return result
